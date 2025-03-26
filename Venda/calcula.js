f9b7e8a8c9727d2f9d56725094cba7cd62cea93a
erick barbosa <erickbsasaki@gmail.com>
2025-03-26 11:06:32 -0300

    Atualizando site

--- Venda/calcula.js
+++ Venda/calcula.js
@@ -121,7 +121,7 @@ function calcularPromocao() {
     const precoVenda = parseFloat(
         document.getElementById("promo-precoVenda").value
     );
-    const imposto = 0.02;    //imposto de 2%
+    const imposto = 0.0108;//imposto de 1,08%
     const comissao = 0.20;
     const taxaPedido = 4;
 
